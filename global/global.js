9476b77e96fb42ebf16b6a6658cb5253cb9fd5ff
cheesequake <dev.cheesequake@gmail.com>
2023-12-06 11:52:20 +0530

    Minor changes to Global
    Plans to update global post exams

--- global/global.js
+++ global/global.js
@@ -1,33 +1,35 @@
 setInterval(()=>{
     const loadTime = new Date();
-    document.getElementById("IST").textContent = ((loadTime.getUTCHours()+5)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes()+30)%60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-    document.getElementById("FINST").textContent = ((loadTime.getUTCHours()+3)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes())).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-    if((loadTime.getUTCHours()-5)<0){
-        document.getElementById("USAST").textContent = ((24+loadTime.getUTCHours()-5)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes())).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    let currentHr = loadTime.getUTCHours();
+    let currentMin = loadTime.getUTCMinutes();
+    document.getElementById("IST").textContent = (Math.ceil((currentHr+5)%24)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin+30)%60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    document.getElementById("FINST").textContent = (Math.ceil((currentHr+2)%24)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    if((currentHr-5)<0){
+        document.getElementById("USAST").textContent = (Math.ceil((24+currentHr-5)%24)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
     }
     else{
-        document.getElementById("USAST").textContent = ((loadTime.getUTCHours()-5)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes())).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+        document.getElementById("USAST").textContent = ((currentHr-5)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
     }
-    document.getElementById("UKST").textContent = ((loadTime.getUTCHours()+1)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes())).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-    document.getElementById("JAPST").textContent = ((loadTime.getUTCHours()+9)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes())).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-    document.getElementById("SAST").textContent = ((loadTime.getUTCHours()+4)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes())).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-    document.getElementById("ESST").textContent = ((loadTime.getUTCHours()+2)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes())).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-    if((loadTime.getUTCHours()-4)<0){
-        document.getElementById("CAST").textContent = ((24+loadTime.getUTCHours()-4)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes())).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-    }
-    else{
-        document.getElementById("CAST").textContent = ((loadTime.getUTCHours()-4)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes())).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-    }
-    document.getElementById("RUST").textContent = ((loadTime.getUTCHours()+3)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes())).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-    document.getElementById("CHST").textContent = ((loadTime.getUTCHours()+8)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes())).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-    document.getElementById("AUST").textContent = ((loadTime.getUTCHours()+10)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes()+30)%60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-    document.getElementById("FRST").textContent = ((loadTime.getUTCHours()+2)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes())).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-    document.getElementById("GERST").textContent = ((loadTime.getUTCHours()+2)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes())).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-    if((loadTime.getUTCHours()-3)<0){
-        document.getElementById("BRST").textContent = ((24+loadTime.getUTCHours()-3)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes())).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    document.getElementById("UKST").textContent = ((currentHr)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    document.getElementById("JAPST").textContent = ((currentHr+9)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    document.getElementById("SAST").textContent = ((currentHr+4)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    document.getElementById("ESST").textContent = ((currentHr+1)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    if((currentHr-5)<0){
+        document.getElementById("CAST").textContent = ((24+currentHr-5)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
     }
     else{
-        document.getElementById("BRST").textContent = ((loadTime.getUTCHours()-3)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes())).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+        document.getElementById("CAST").textContent = ((currentHr-4)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
     }
-    document.getElementById("MAST").textContent = ((loadTime.getUTCHours()+8)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((loadTime.getUTCMinutes())).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    document.getElementById("RUST").textContent = ((currentHr+3)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    document.getElementById("CHST").textContent = ((currentHr+8)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    document.getElementById("AUST").textContent = ((currentHr+10)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin+30)%60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    document.getElementById("FRST").textContent = ((currentHr+2)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    document.getElementById("GERST").textContent = ((currentHr+1)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    if((currentHr-3)<0){
+        document.getElementById("BRST").textContent = ((24+currentHr-3)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    }
+    else{
+        document.getElementById("BRST").textContent = ((currentHr-3)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    }
+    document.getElementById("MAST").textContent = ((currentHr+8)%24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+":"+((currentMin)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
 },1000)
